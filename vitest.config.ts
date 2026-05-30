a472a8b090cba372dcf6e0b60ab9dc3785e591b2
sylhare <sylhare@outlook.com>
2026-05-29 21:15:34 -0400

    Update lock file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import yaml from 'js-yaml';
-import path from 'path';
-import { defineConfig } from 'vitest/config';
 import type { Plugin } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 function yamlPlugin(): Plugin {
   return {
